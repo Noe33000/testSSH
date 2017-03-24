be12346537d63876d0f8cc954625e1b623886d49
Noe33000 <champignyn@gmail.com>
2017-03-24 11:58:49 +0100

    Mise en place du bouton

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('test');
+var p = document.getElementById("color");
+
+if(document.body.style.backgroundColor = 'white'){
+    p.onclick = function() { document.body.style.backgroundColor = "red"; };   
+}
+
+if(document.body.style.backgroundColor = 'red'){
+    p.onclick = function() { document.body.style.backgroundColor = "white"; };   
+}
